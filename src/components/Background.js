dbdc99e8f27943b84ab61ced832f00296a76a54a
magdazelena <magda.zelezik@gmail.com>
2020-10-15 21:23:53 +0200

    Switch to react fiber

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,19 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Canvas } from 'react-three-fiber';
 import THREE from '../3D/three';
-import SimplexNoise from 'simplex-noise';
+import BackgroundAnimation from './BackgroundAnimation';
+
 function Background(props) {
-  const container = useRef();
   const [position, setPosition] = useState('back');
-  let cols, rows, scene, renderer, camera, mesh;
-  let scale = 20;
-  let width = 2000;
-  let height = 2000;
-  let zArray = [];
-  var simplex = new SimplexNoise();
-  let geometry;
-  let light1, light2, light3;
-  var positionChange = width / 3;
-  var frontLight;
+
+
   useEffect(() => {
     if (props.isForeground) {
       setPosition('front');
@@ -21,122 +14,24 @@ function Background(props) {
       setPosition('back');
     }
   }, [props.isForeground]);
-  useEffect(() => {
-    if (container.current) {
-      setup();
-      draw();
-    }
-    // eslint-disable-next-line 
-  }, [container.current]);
-  let setup = () => {
-    const canvas = document.createElement('canvas');
-    cols = width / scale;
-    rows = height / scale;
-    let ioff = f;
-    for (let i = 0; i < cols; i++) {
-      let joff = f;
-      for (let j = 0; j < rows; j++) {
-        zArray.push(map_range(simplex.noise2D(ioff, joff), 0, 1, -5, 5));
-        joff += .1;
-      }
-      ioff += .1;
-    }
-    //scene
-    scene = new THREE.Scene();
-    //renderer
-    renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(0x000000, 0);
-    renderer.shadowMap.enabled = true;
-    renderer.setPixelRatio(window.devicePixelRatio);
-    container.current.appendChild(renderer.domElement);
-    //camera
-    camera = new THREE.PerspectiveCamera(
-      200,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
-    camera.position.z = 100;
-    camera.position.x = 100;
-    camera.position.y = -100;
-
-    geometry = new THREE.PlaneGeometry(width * 3, height * 3, cols, rows)
-
-    var material = new THREE.MeshStandardMaterial({
-      color: 0x769fb6,
-      wireframe: true,
-      transparent: true,
-      opacity: 0.8
-    });
-    geometry.verticesNeedUpdate = true;
-    mesh = new THREE.Mesh(geometry, material);
-    mesh.rotateX(Math.PI / 26)
-    scene.add(mesh);
-    //lights
-    var light = new THREE.HemisphereLight(0x769fb6, 0x000000, 2);
-    scene.add(light);
-    light1 = new THREE.DirectionalLight(0xff0040, 2);
-    light1.position.z = 102;
-    scene.add(light1);
-    light2 = new THREE.DirectionalLight(0x0040ff, 1);
-    light2.position.z = 1000;
-    scene.add(light2);
-
-    light3 = new THREE.PointLight(0x80ff80, 1, width, 2);
-    light3.position.z = 253;
-    scene.add(light3);
-    frontLight = new THREE.DirectionalLight(0xffffff, 1);
-    frontLight.position.set(3000, 300, 3000).normalize(); // just a direction. you can normalize
-    scene.add(frontLight);
-  }
-  let f = 0;
-  let rmapped = 0;
-  let draw = () => {
-    requestAnimationFrame(draw);
-    var time = Date.now() * 0.0005;
-    f += .01;
-    let ioff = f;
-    zArray = [];
-    for (let i = 0; i < cols; i++) {
-      let joff = 0;
-      for (let j = 0; j < rows; j++) {
-        zArray.push(map_range(simplex.noise2D(ioff, joff), 0, 1, -10, 10));
-        joff += .1;
-      }
-      ioff += .1;
-    }
-    for (let x = 0; x < geometry.vertices.length; x++) {
-      geometry.vertices[x].z = zArray[x];
-    }
-    geometry.verticesNeedUpdate = true;
-    light1.position.x = Math.sin(time * 0.1) * positionChange;
-    light1.position.y = Math.sin(time * 0.4) * positionChange;
-    light2.position.x = Math.cos(time * 0.3) * positionChange;
-    light2.position.y = Math.sin(time * 0.2) * positionChange;
-
-    light3.position.x = Math.sin(time * 0.9) * positionChange;
-    light3.position.y = Math.sin(time * 0.5) * positionChange;
-    var h = rmapped * 0.01 % 1;
-    var s = 0.5;
-    var l = 0.5;
-    frontLight.color.setHSL(h, s, l);
-
-    rmapped++;
-    if (position === 'front') {
-      mesh.rotation.set(new THREE.Vector3(0, 0, Math.PI / 2));
-    }
-    renderer.render(scene, camera);
-
-  }
 
   return (
-    <div className={`Background ${position}`} ref={container}>
+    <div className={`Background ${position}`}>
+      <Canvas
+        camera={
+          {
+            fov: 100,
+            aspect: window.innerWidth / window.innerHeight,
+            near: 0.1,
+            far: 1000,
+            position: new THREE.Vector3(0, -120, 200)
+          }
+        }>
+        <BackgroundAnimation />
+
+      </Canvas>
     </div>
   );
 }
 
 export default Background;
-function map_range(value, low1, high1, low2, high2) {
-  return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
-}
---
+++ src/components/BackgroundAnimation.js
@@ -0,0 +1,97 @@
+import React, { useRef } from 'react';
+import { useFrame } from 'react-three-fiber';
+import THREE from '../3D/three';
+
+import SimplexNoise from 'simplex-noise';
+
+function BackgroundAnimation(props) {
+  const scale = 20;
+  const width = 2000;
+  const height = 2000;
+  const cols = width / scale;
+  const rows = height / scale;
+  let f = 0;
+  let zArray = [];
+  const simplex = new SimplexNoise();
+
+  const mesh = useRef();
+  const frontLight = useRef();
+  const redLight = useRef();
+  const purpleLight = useRef();
+  const greenLight = useRef();
+  let rmapped = 0;
+  const positionChange = 500;
+
+  const setup = () => {
+
+    let ioff = f;
+    for (let i = 0; i < cols; i++) {
+      let joff = f;
+      for (let j = 0; j < rows; j++) {
+        zArray.push(map_range(simplex.noise2D(ioff, joff), 0, 1, -5, 5));
+        joff += .1;
+      }
+      ioff += .1;
+    }
+
+
+  }
+  setup();
+  useFrame((state, time) => {
+    f += .01;
+    let ioff = f;
+    zArray = [];
+    for (let i = 0; i < cols; i++) {
+      let joff = 0;
+      for (let j = 0; j < rows; j++) {
+        zArray.push(map_range(simplex.noise2D(ioff, joff), 0, 1, -18, 18));
+        joff += .1;
+      }
+      ioff += .1;
+    }
+    for (let x = 0; x < mesh.current.geometry.vertices.length; x++) {
+      mesh.current.geometry.vertices[x].z = zArray[x];
+    }
+    mesh.current.geometry.verticesNeedUpdate = true;
+    redLight.current.position.x = Math.sin(time) * positionChange;
+    redLight.current.position.y = Math.sin(time) * positionChange;
+    purpleLight.current.position.x = Math.cos(time) * positionChange / 2;
+    purpleLight.current.position.y = Math.sin(time) * positionChange / 2;
+
+    greenLight.current.position.x = Math.sin(time) * positionChange / 3;
+    greenLight.current.position.y = Math.sin(time) * positionChange / 3;
+    var h = rmapped * 0.01 % 1;
+    var s = 0.5;
+    var l = 0.5;
+    frontLight.current.color.setHSL(h, s, l);
+
+    rmapped++;
+  })
+  return (
+    <group>
+      <mesh
+        {...props}
+        ref={mesh}
+        rotateX={Math.PI / 16}>
+        <planeGeometry args={[width * 3, height * 3, cols, rows]} />
+        <meshStandardMaterial args={[{
+          color: 0x769fb6,
+          wireframe: true,
+          transparent: true,
+          opacity: 0.8
+        }]} />
+      </mesh>
+      <hemisphereLight color={0x769fb6} groundColor={0x000000} intensity={1} />
+      <directionalLight ref={redLight} color={0xff0040} intensity={1} position={new THREE.Vector3(0, 0, 102)} />
+      <directionalLight ref={purpleLight} color={0x0040ff} intensity={2} position={new THREE.Vector3(0, 0, 200)} />
+      <pointLight ref={greenLight} color={0x80ff80} distance={2000} intensity={1} decay={2} position={new THREE.Vector3(0, 0, 153)} />
+      <directionalLight ref={frontLight} color={0xffffff} intensity={1} decay={2} position={new THREE.Vector3(300, 300, 3000)} />
+    </group>
+  )
+}
+
+export default BackgroundAnimation;
+
+function map_range(value, low1, high1, low2, high2) {
+  return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+}
